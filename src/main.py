41bcd9219c6c60159bb479d530048649779b56ab
arybs <arkadiusz.rybski@gmail.com>
2021-01-16 15:44:58 +0100

    zmiany

--- src/main.py
+++ src/main.py
@@ -7,27 +7,36 @@ import matplotlib.pyplot as pyplot
 from matplotlib.animation import FuncAnimation
 
 from mesh import mesh
+from visualization import animation
 
 
 TempL=300
 TempR=900
 size=1
 elem_num=200
-max_iter=100
+max_iter=int(5e04)
+time_delta=0.1
 
 def main():
     print ("Witaj w świecie klałd kopjutingu")
 
     # Kontekst sparka
-    context = pyspark.SparkContext('local[*]')
+    context = pyspark.SparkContext()
     context.addPyFile("src/point.py")
 
     my_mesh = mesh(size, elem_num, TempL, TempR)
-    
-    print("Proba numpy\n", a)
-    for step in range(100):
-        my_mesh.step_mesh(0.1, context)
+
+    data=[]
+    temp, _ = np.meshgrid(my_mesh.data_return(), my_mesh.data_return())
+    data.append(temp)
+    for step in range(max_iter):
+        my_mesh.step_mesh(time_delta, context)
         my_mesh.update_mesh()
+        temp, _ = np.meshgrid(my_mesh.data_return(), my_mesh.data_return())
+        data.append(temp)
+    
+    context.stop()
+    animation(data)
     print(my_mesh)
     sys.exit()
 
@@ -39,10 +48,11 @@ if __name__ == "__main__":
 
 # vvv ten syf zostawiam do wglądu
 
-sc = pyspark.SparkContext('local[*]')
+sc = pyspark.SparkContext()
 
 txt = sc.textFile('file:////usr/share/doc/python3.8/copyright')
 print(txt.count())
 
 python_lines = txt.filter(lambda line: 'python' in line.lower())
 print(python_lines.count())
+sc.stop()

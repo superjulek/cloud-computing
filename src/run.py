4ac7bde4dc56feb7523cb725e743504cf68b04e5
arybs <arkadiusz.rybski@gmail.com>
2021-01-16 15:56:15 +0100

    AWSworking

--- src/run.py
+++ src/run.py
@@ -9,19 +9,34 @@ from config import constants, initials, simulation, stepping
 
 def run():
 
+    spark = SparkSession\
+    .builder\
+    .appName("HeatFlow")\
+    .getOrCreate()
+    
+    #context.addPyFile("src/point.py")
+    spark.sparkContext.addPyFile("src/point.py")
+    spark.sparkContext("src/config.py")
+
     # pyspark context
-    context = pyspark.SparkContext('local[*]')
-    context.addPyFile("src/point.py")
-    context.addPyFile("src/config.py")
+    #context = pyspark.SparkContext('local[*]')
+    #context.addPyFile("src/point.py")
+    #context.addPyFile("src/config.py")
 
     my_mesh = mesh(constants.length_val, (int)(constants.length_val /  stepping.space_delta))
+    data =[]
     for step in range(simulation.max_step_num):
         diff = my_mesh.step_mesh(stepping.time_delta, context)
         if (diff < simulation.expected_diff):
             print('Finished after ' + str(step) + ' steps')
             break
+        data.append(my_mesh.data_return())
         my_mesh.update_mesh()
+
     print(my_mesh)
+     with open('Results.txt', 'w') as f:
+        for item in data:
+            f.write("%s\n" % item)
 
     if simulation.run_check is True:
         print ('Running check simulation with parameters:')
